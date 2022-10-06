249f124b541827e146839b24d9e570f3311eb3e8
jeff <odhiambo47@gmail.com>
2022-10-06 18:17:48 +0300

    made app component with its function

---
+++ budget-planner/src/Navbar.js
@@ -0,0 +1,17 @@
+
+
+function Navbar() {
+    return (
+    <nav className="nav">
+        <a href="/" className="site-title">Oty's Budget Planner</a>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/social">Social</a>
+            </li>
+        </ul>
+    </nav>
+)}
+
+export default Navbar;
