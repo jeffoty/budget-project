247b11cba7b742275e1c4f18c0ee881a39a34d08
jeff <odhiambo47@gmail.com>
2022-10-07 13:49:03 +0300

    added viewbudget component

---
+++ budget-planner/src/components/ViewBudget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ViewBudget = (props) => {
+	return (
+		<>
+			<span>Budget: KSH{props.budget}</span>
+			<button type='button' class='btn btn-primary' onClick={props.handleEditClick}>
+				Edit
+			</button>
+		</>
+	);
+};
